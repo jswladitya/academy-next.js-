164019886b9d34e84b672a92896acd145b2a30c6
jswladitya <jswladitya0403@gmail.com>
2025-01-27 20:02:12 +0530

    project completed

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,37 +8,40 @@ function Navbar({ className }: { className?: string }) {
   const [active, setActive] = useState<string | null>(null);
   return (
     <div
-      className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
+    className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
     >
-      <Menu setActive={setActive}>
-        <Link href={"/"}>
-          <MenuItem
-            setActive={setActive}
-            active={active}
-            item="Home"
-          ></MenuItem>
-        </Link>
-
-        <MenuItem setActive={setActive} active={active} item="Our Courses">
-          <div className="flex flex-col space-y-4 text-sm">
-            <HoveredLink href="/all-courses">All Courses</HoveredLink>
-            <HoveredLink href="/music-theory">Basic Music Theory</HoveredLink>
-            <HoveredLink href="/advanced-composition">Advanced Composition</HoveredLink>
-            <HoveredLink href="/songwriting">Songwriting</HoveredLink>
-            <HoveredLink href="/music-production">Music Production</HoveredLink>
-          </div>
-        </MenuItem>
-
-        <Link href={"/contact"}>
-          <MenuItem
-            setActive={setActive}
-            active={active}
-            item="Contact Us"
-          ></MenuItem>
-        </Link>
-      </Menu>
+        <Menu setActive={setActive}>
+            <Link href={"/"}>
+            <MenuItem setActive={setActive} active={active} item="Home">
+            
+            </MenuItem>
+            </Link>
+            <MenuItem
+            setActive={setActive} active={active} item="Our Courses"
+            >
+               <div className="flex flex-col space-y-4 text-sm">
+               <HoveredLink href="/courses">All Courses</HoveredLink>
+            <HoveredLink href="/courses">
+              Basic Music Theory
+            </HoveredLink>
+            <HoveredLink href="/courses">
+              Advanced Composition
+            </HoveredLink>
+            <HoveredLink href="/courses">Songwriting</HoveredLink>
+            <HoveredLink href="/courses">
+              Music Production
+            </HoveredLink>
+               </div>
+            </MenuItem>
+            <Link href={"/contact"}>
+            {/* ye actually folder ka route likhkte he nextjs me */}
+            <MenuItem setActive={setActive} active={active} item="Contact Us">
+            
+            </MenuItem>
+            </Link>
+        </Menu>
     </div>
-  );
+  )
 }
 
 export default Navbar;
